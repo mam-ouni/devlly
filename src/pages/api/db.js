77caced57da8f002e71e61c40f742678ada7f719
yahia <yahia.slimani@univ-constantine2.dz>
2024-07-04 13:35:24 +0100

    db

--- src/pages/api/db.js
+++ src/pages/api/db.js
@@ -1,11 +1,18 @@
-import mysql from 'mysql2/promise'
+import mysql from 'mysql2/promise';
 
-export default function Connection(){
-     const db = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'yahia',
-        database: 'devly'
-     })
-     return db;
+export default async function Connection() {
+    try {
+        console.log(process.env.DB_HOST, process.env.DB_USER, process.env.DB_PASSWORD, process.env.DB_NAME);
+        const db = await mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'yahia',
+            database:'devly'
+        });
+        console.log('Database connected successfully');
+        return db;
+    } catch (error) {
+        console.error('Database connection error:', error);
+        throw error;
+    }
 }
