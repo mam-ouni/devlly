760ad038e690588d26fcc972f0b63cc9aa6e8478
mam-ouni <salah.aissaoui@univ-constantine2.dz>
2024-07-04 21:13:58 +0200

    update

--- src/pages/api/updateAppointment.js
+++ src/pages/api/updateAppointment.js
@@ -7,15 +7,15 @@ export default async function updateAppointment(req, res) {
         console.log(req.body.id);
 
         if (status === 'cancel') {
-            const result = await sql(`UPDATE appointment SET status = 'cancelled' WHERE id_appointment = ${id}`);
+            const result = await sql`UPDATE appointment SET status = 'cancelled' WHERE id_appointment = ${id}`;
             console.log(result);
             res.status(200).json({ message: "Appointment cancelled successfully" });
         } else if (status === 'confirm') {
-            const result = await sql(`UPDATE appointment SET status = 'confirmed' WHERE id_appointment = ${id}`);
+            const result = await sql`UPDATE appointment SET status = 'confirmed' WHERE id_appointment = ${id}`;
             console.log(result);
             res.status(200).json({ message: "Appointment confirmed successfully" });
         } else {
-            const result = await sql(`DELETE FROM appointment WHERE id_appointment = ${id}`);
+            const result = await sql`DELETE FROM appointment WHERE id_appointment = ${id}`;
             console.log(result);
             res.status(200).json({ message: 'Appointment deleted successfully' });
         }
